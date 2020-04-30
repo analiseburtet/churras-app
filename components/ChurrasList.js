730dca25d418455d6529953ff51c4fc5a5f5421f
analiseburtet <analiseburtet@gmail.com>
2020-04-29 23:36:20 -0300

    adicionar funcionalidade de incluir guests, remover/ editar evento

--- components/ChurrasList.js
+++ components/ChurrasList.js
@@ -1,43 +1,68 @@
-import React, { Component, useState, useEffect } from "react";
-import { Text, View, FlatList, StyleSheet, SafeAreaView, AsyncStorage } from 'react-native';
-import NewChurras from './NewChurras'
-import { displayData } from './Form'
-import Moment from 'moment'
+import React, { Component, useState, useEffect } from 'react';
+import {
+  Text,
+  View,
+  FlatList,
+  StyleSheet,
+  SafeAreaView,
+  AsyncStorage,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import NewChurras from './NewChurras';
+import { displayData } from './Form';
+import Moment from 'moment';
 
-function Item({ title, date }) {
+function Item({ title, date, key }) {
   return (
     <View style={styles.item}>
-      <Text style={styles.date}>{date}</Text>
+      <TouchableOpacity>
+        <Image style={styles.deleteBin} source={require('../assets/can.png')} />
+      </TouchableOpacity>
+      <Text style={styles.date}>{Moment.unix(date).format('DD/MM')}</Text>
       <Text style={styles.title}>{title}</Text>
+      <TouchableOpacity>
+        <Image
+          style={styles.deleteBin}
+          source={require('../assets/edit.png')}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
 
 export default function ChurrasList() {
-  const [ data, setData ] = useState([])
-  const [ item, setItem ] = useState([])
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
-      const retrievedData = await displayData()
-      if(retrievedData && retrievedData.length) setData(retrievedData)
-    }
-    getData()
-  }, [])
+      const retrievedData = (await displayData()).map(([, value]) =>
+        JSON.parse(value)
+      );
+      if (retrievedData && retrievedData.length) {
+        setData(retrievedData);
+      }
+    };
+    getData();
+  }, []);
 
-  const onSubmitForm = async (values) => {  
-    const unixTimestamp = Moment(values.date).format('DD-MM').toString();
+  const onSubmitForm = async values => {
+    const unixTimestamp = Moment(values.date)
+      .unix()
+      .toString();
     const finalValues = { ...values, date: unixTimestamp };
     await AsyncStorage.setItem(unixTimestamp, JSON.stringify(finalValues));
     setData([...data, finalValues]);
-  }
+  };
 
   return (
     <View>
-       <SafeAreaView>
+      <SafeAreaView>
         <FlatList
           data={data}
-          renderItem={({ item }) => <Item title={item.title} date={item.date} />}
+          renderItem={({ item }) => (
+            <Item title={item.title} date={item.date} />
+          )}
           keyExtractor={item => item.id}
         />
       </SafeAreaView>
@@ -56,39 +81,43 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-      shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5
+    elevation: 5,
   },
   title: {
-    flex: 1
+    flex: 1,
   },
   date: {
     fontSize: 20,
     flex: 1,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   addEvent: {
     flexDirection: 'row',
     justifyContent: 'center',
     textAlign: 'center',
     backgroundColor: 'gainsboro',
-    borderRadius: 20, 
+    borderRadius: 20,
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-      shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5
+    elevation: 5,
+  },
+  deleteBin: {
+    width: 20,
+    height: 20,
   },
 });
--- components/Form.js
+++ components/Form.js
@@ -1,16 +1,52 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Button, SafeAreaView, TextInput, AsyncStorage } from 'react-native';
-import DatePicker from 'react-native-datepicker'
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  TextInput,
+  AsyncStorage,
+  FlatList,
+  CheckBox,
+  ScrollView
+} from 'react-native';
+import DatePicker from 'react-native-datepicker';
 import { Formik } from 'formik';
-import Moment from 'moment'
+import Moment from 'moment';
+
+function Item({ name, amount, isVegan, contributed, drink }) {
+  return (
+    <View style={styles.list}>
+      <CheckBox value={contributed} onValueChange={!contributed} />
+      <Text>{name}</Text>
+      <Text>$ {amount}</Text>
+      <CheckBox value={isVegan} onValueChange={!isVegan} />
+      <CheckBox value={drink} onValueChange={!drink} />
+    </View>
+  );
+}
 
 const Form = props => (
   <Formik
-    initialValues={{ title : '' , date: Moment(), totalAmount: 0, people: [ {name: "name", amount: 0, isVegan: false, contributed: false} ] }}
-    onSubmit={props.onSubmitForm}
-  >
+    initialValues={{
+      title: '',
+      date: Moment(),
+      totalAmount: 0,
+      people: [
+        {
+          name: 'Ana',
+          amount: 20,
+          isVegan: true,
+          contributed: true,
+          drink: true,
+        },
+      ],
+    }}
+    onSubmit={props.onSubmitForm}>
     {({ handleChange, handleBlur, handleSubmit, values }) => (
       <View>
+      <ScrollView>
         <TextInput
           placeholder="Title"
           onChangeText={handleChange('title')}
@@ -26,13 +62,36 @@ const Form = props => (
           mode="date"
           onDateChange={handleChange('date')}
         />
+        <View style={styles.list}>
+          <Text>Paid</Text>
+          <Text>Guest</Text>
+          <Text>Amount</Text>
+          <Text>Vegan</Text>
+          <Text>Drinks</Text>
+        </View>
+        <SafeAreaView>
+          <FlatList
+            data={values.people}
+            keyExtractor={key => values.date}
+            renderItem={({ item }) => (
+              <Item
+                name={item.name}
+                amount={item.amount}
+                isVegan={item.isVegan}
+                contributed={item.contributed}
+                drink={item.drink}
+              />
+            )}
+          />
+        </SafeAreaView>
         <Button onPress={handleSubmit} title="Save" />
+      </ScrollView>
       </View>
     )}
   </Formik>
 );
 
-export default Form
+export default Form;
 
 export const displayData = async () => {
   let objects = null;
@@ -41,12 +100,18 @@ export const displayData = async () => {
     try {
       keys = await AsyncStorage.getAllKeys();
       objects = await AsyncStorage.multiGet(keys);
+      return objects;
+    } catch (error) {
+      console.log(error);
     }
-    catch(error) {
-      console.log(error)
-    }
-  } 
-    catch (error) {
+  } catch (error) {
     console.log(error);
   }
-}
+};
+
+const styles = StyleSheet.create({
+  list: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
